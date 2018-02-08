9b54b1c6cf1f576d45c67fecf142ac2d6116d9c9
Aleksey Goncharov <alexwolk01@gmail.com>
2018-02-08 15:53:21 +0300

    work progress

--- glagne.go
+++ glagne.go
@@ -230,8 +230,14 @@ func GenerateRunScript() error {
 
 func GenerateCustomConfigurationPhp() string {
 	//This function generate custom config php.ini
-
-	return ""
+	parameters := make(map[string]string)
+	parameters["UploadMaxFilesize"] = "100M"
+	parameters["PostMaxSize"] = "100M"
+	parameters["MemoryLimit"] = "128M"
+	phpConfigure := "RUN " + "echo \"upload_max_filesize =  " + parameters["UploadMaxFilesize"] + "\" >> ${php_vars} &&\\\n"
+	phpConfigure += "echo \"post_max_size =  " + parameters["PostMaxSize"] + "\" >> ${php_vars} &&\\\n"
+	phpConfigure += "echo \"memory_limit =  " + parameters["MemoryLimit"] + "\" >> ${php_vars} &&\\\n"
+	return phpConfigure
 }
 func Debian(phpModules []interface{},
 	ModulesNopecl []string,
